49431e44da10ad9989689b1f71386e11a8ca8d4c
xFlowDev <f.loed@web.de>
2017-04-11 11:26:22 +0200

    Debug Info now drawn to Screen

--- SfmlTest/SfmlTest/Game.cpp
+++ SfmlTest/SfmlTest/Game.cpp
@@ -1,22 +1,20 @@
 #include "Game.hpp"
 
-#define TIME_POINT std::chrono::time_point<std::chrono::high_resolution_clock>
-#define DURATION std::chrono::duration<double>
-
 Game::Game() {
 	GameWindow.create(sf::VideoMode(WIDTH, HEIGHT), "2D RPG Rougelike");
 	GameState = Menu;
+	setDebugText();
 }
 Game::~Game() {
 	GameWindow.close();
 }
 
 void Game::GameLoop() {
-	Timer = std::chrono::high_resolution_clock::now();
-	double dt = 0.016666666666666667;
+	timer = std::chrono::high_resolution_clock::now();
+	dt = 0.016666666666666667;
 
-	TIME_POINT currentTime = std::chrono::high_resolution_clock::now();
-	double accumulator = 0.0;
+	currentTime = std::chrono::high_resolution_clock::now();
+	accumulator = 0.0;
 
 	while (GameWindow.isOpen())
 	{
@@ -26,11 +24,11 @@ void Game::GameLoop() {
 				GameWindow.close();
 		}
 
-		TIME_POINT newTime = std::chrono::high_resolution_clock::now();
-		DURATION frameTime = newTime - currentTime;
+		newTime = std::chrono::high_resolution_clock::now();
+		frameTime = newTime - currentTime;
 		currentTime = newTime;
 		accumulator += frameTime.count();
-		
+
 		while (accumulator >= dt) {
 			Update();
 			accumulator -= dt;
@@ -40,13 +38,12 @@ void Game::GameLoop() {
 		Draw();
 		FPS++;
 
-		TIME_POINT now = std::chrono::high_resolution_clock::now();
-		DURATION diff = now - Timer;
-		if (diff > std::chrono::seconds(1))	{
-			Timer += std::chrono::milliseconds(1000);
-			//TODO diese Daten in eine Debug-Anzeige im Screen zeichnen
-			//Mit möglichkeit die Anzeige zu aktivieren/deaktivieren
-			std::cout << "FPS: " << FPS << " Updates: " << Updates << std::endl;
+		now = std::chrono::high_resolution_clock::now();
+		diff = now - timer;
+		if (diff > std::chrono::seconds(1)) {
+			timer += std::chrono::milliseconds(1000);
+			UpdatesText.setString("UPS: " + std::to_string(Updates));
+			FpsText.setString("FPS: " + std::to_string(FPS));
 			FPS = 0;
 			Updates = 0;
 		}
@@ -79,7 +76,12 @@ void Game::Update() {
 //Jede Klasse zeichnet ihren eigenen Kram
 void Game::Draw() {
 	GameWindow.clear();
-	drawDebugInfo();
+	
+	//Draw Debug Info
+	GameWindow.draw(FpsText);
+	GameWindow.draw(UpdatesText);
+	
+
 	//Referenz von GameWindow wird übergegeben
 	//ein Objekt zeichnet alles
 	if (GameState == Menu)
@@ -90,7 +92,18 @@ void Game::Draw() {
 	GameWindow.display();
 }
 
-//zeichnet FPS, Updates und weiteres als Text in as GameWindow
-void Game::drawDebugInfo() {
-
+void Game::setDebugText() {
+	if (!DebugFont.loadFromFile("Assets\\Fonts\\VCR_OSD_MONO.ttf")) {
+		std::cerr << "Error Loading Font" << std::endl;
+	}
+	else {
+		FpsText.setFont(DebugFont);
+		FpsText.setCharacterSize(DebugFontSize);
+		FpsText.setFillColor(DebugTextColor);
+		FpsText.setPosition(FpsTextPosition);
+		UpdatesText.setFont(DebugFont);
+		UpdatesText.setCharacterSize(DebugFontSize);
+		UpdatesText.setFillColor(DebugTextColor);
+		UpdatesText.setPosition(UpdatesTextPosition);
+	}
 }
--- SfmlTest/SfmlTest/Game.hpp
+++ SfmlTest/SfmlTest/Game.hpp
@@ -11,6 +11,9 @@
 #include "GameScreen.hpp"
 #include "MenuScreen.hpp"
 
+#define TIME_POINT std::chrono::time_point<std::chrono::high_resolution_clock>
+#define DURATION std::chrono::duration<double>
+
 class Game {
 public:
 	Game();
@@ -23,16 +26,30 @@ private:
 
 	GameState GameState;
 
-	std::chrono::high_resolution_clock::time_point Timer;
+	//timer: Für das Messen der Zeit vom ersten Starten des GameLoops
+	//currentTime: enthällt immer die Zeit zu der die Updates gemessen werden
+	//newTime: immer der Zeitpunkt am Anfang des GameLoop
+	//now: Für das Abmessen einer Sekunde um die Updates/s und FPS zu messen
+	TIME_POINT timer, currentTime, newTime, now;
+	//frameTime: Die Zeit die ein Frame/Update benötigt
+	//diff: Zum Abmessen einer Sekunde, wenn diff größer als 1s ist dann wird die Anzahl der UPS und FPS ausgegeben
+	DURATION frameTime, diff;
+	double dt, accumulator;
+	int Updates, FPS;
 
-	int Updates;
-	int FPS;
+	const int DebugFontSize = 18;
+	sf::Color DebugTextColor = sf::Color::White;
+	sf::Font DebugFont;
+	sf::Vector2f FpsTextPosition = sf::Vector2f(0,0);
+	sf::Text FpsText;
+	sf::Vector2f UpdatesTextPosition = sf::Vector2f(0, DebugFontSize + 6);
+	sf::Text UpdatesText;
 
-
+	
 	void Update();
 	void Draw();
 
-	void drawDebugInfo();
+	void setDebugText();
 
 	MenuScreen menuScreen;
 	GameScreen gameScreen;
